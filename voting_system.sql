149da03deaafb73f868e057b1c05685f8cda8527
SpurthiKoti <kspurthid2@gmail.com>
2024-07-01 14:37:43 +0530

    online voting_system.sql

--- voting_system.sql
+++ voting_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2023 at 04:28 PM
+-- Generation Time: Jul 06, 2024 at 04:40 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
